f6938c8b79195f03ff1b5b511bdcabfcc6a8f5dc
Pilathraj <pilathraj@gmail.com>
2018-04-25 17:57:22 +0530

    syntax error fixed

--- runkit_common.c
+++ runkit_common.c
@@ -36,7 +36,7 @@ void ensure_all_objects_of_class_have_magic_methods(zend_class_entry *ce) {
 	  GC_FLAGS(object) |= IS_OBJ_USE_GUARDS;  // see zend_object_std_init
 	  break;
 	}
-	PHP_RUNKIT_ITERATE_THROUGH_OBJECTS_STORE_END
+	PHP_RUNKIT_ITERATE_THROUGH_OBJECTS_STORE_END;
 }
 /* }}} */
 
